8d97263b78a9302a23c7ce30736e29dd905f708f
BirasaDivine <b.divine@alustudent.com>
2024-09-29 19:15:27 +0200

    Changed Var

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const fun = 'C is fun';
-const python = 'Python is cool';
-const java = 'JavaScript is amazing';
+const fun = "C is fun";
+const python = "Python is cool";
+const java = "JavaScript is amazing";
 console.log(fun);
 console.log(python);
 console.log(java);
